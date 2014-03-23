82ce65a5f18e811deb6f10c0492480f115320fae
wrobell <wrobell@pld-linux.org>
2014-03-23 22:21:42 +0000

    - added support for osm cycling layer

--- ModestMaps/OpenStreetMap.py
+++ ModestMaps/OpenStreetMap.py
@@ -11,14 +11,20 @@ from math import pi
 from .Core import Coordinate
 from .Geo import MercatorProjection, deriveTransformation
 from .Providers import IMapProvider
-
 from . import Tiles
 
+FMT_MAP = 'http://tile.openstreetmap.org/%d/%d/%d.png'
+FMT_CYCLE = 'http://tile.opencyclemap.org/cycle/%d/%d/%d.png'
+
 class Provider(IMapProvider):
-    def __init__(self):
+
+    def __init__(self, layer='map'):
         # the spherical mercator world tile covers (-π, -π) to (π, π)
         t = deriveTransformation(-pi, pi, 0, 0, pi, pi, 1, 0, -pi, -pi, 0, 1)
         self.projection = MercatorProjection(0, t)
+        self.fmt = FMT_MAP
+        if layer == 'cycle':
+            self.fmt = FMT_CYCLE
 
     def tileWidth(self):
         return 256
@@ -27,7 +33,7 @@ class Provider(IMapProvider):
         return 256
 
     def getTileUrls(self, coordinate):
-        return ('http://tile.openstreetmap.org/%d/%d/%d.png' % (coordinate.zoom, coordinate.column, coordinate.row),)
+        return (self.fmt % (coordinate.zoom, coordinate.column, coordinate.row),)
 
 if __name__ == '__main__':
     import doctest
