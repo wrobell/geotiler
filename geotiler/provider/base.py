d0c951a3fd721cb41ed6d3e3062defa8f5209cc8
wrobell <wrobell@pld-linux.org>
2014-03-30 17:15:07 +0100

    - removed unused code

--- geotiler/provider/base.py
+++ geotiler/provider/base.py
@@ -4,10 +4,6 @@ from math import pi, pow
 from ..Core import Coordinate
 from ..Geo import LinearProjection, MercatorProjection, deriveTransformation
 
-ids = (
-    'MICROSOFT_ROAD', 'MICROSOFT_AERIAL', 'MICROSOFT_HYBRID', 'YAHOO_ROAD',
-    'YAHOO_AERIAL', 'YAHOO_HYBRID', 'BLUE_MARBLE', 'OPEN_STREET_MAP'
-)
 
 class IMapProvider(object):
     def __init__(self):
