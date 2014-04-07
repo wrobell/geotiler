5480a534298311a4d8de36636df98502ef5479f4
wrobell <wrobell@pld-linux.org>
2014-04-08 00:23:52 +0100

    - added license header to example files

--- examples/ex-basemap.py
+++ examples/ex-basemap.py
@@ -1,4 +1,33 @@
-# Matplotlib Basemap Toolkit example
+#
+# GeoTiler - library to create maps using tiles from a map provider
+#
+# Copyright (C) 2014 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# This file incorporates work covered by the following copyright and
+# permission notice (restored, based on setup.py file from
+# https://github.com/stamen/modestmaps-py):
+#
+#   Copyright (C) 2007-2013 by Michal Migurski and other contributors
+#   License: BSD
+#
+
+"""
+Matplotlib Basemap Toolkit example.
+"""
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
