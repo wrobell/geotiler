d1e5c544b9a66236853c920819e92ddbfe39c510
wrobell <wrobell@pld-linux.org>
2014-04-08 00:27:44 +0100

    - added matplotlib example

---
+++ examples/ex-matplotlib.py
@@ -0,0 +1,64 @@
+#
+# GeoTiler - library to create maps using tiles from a map provider
+#
+# Copyright (C) 2014 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# This file incorporates work covered by the following copyright and
+# permission notice (restored, based on setup.py file from
+# https://github.com/stamen/modestmaps-py):
+#
+#   Copyright (C) 2007-2013 by Michal Migurski and other contributors
+#   License: BSD
+#
+
+"""
+Matplotlib example.
+"""
+
+import matplotlib.pyplot as plt
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import geotiler
+
+bbox = 11.78560, 46.48083, 11.79067, 46.48283
+
+fig = plt.figure(figsize=(10, 10))
+ax = plt.subplot(111)
+
+#
+# download background map using OpenStreetMap
+#
+mm = geotiler.Map(extent=bbox, zoom=18)
+
+img = geotiler.render_map(mm)
+ax.imshow(img)
+
+#
+# plot 3 custom points
+#
+x0, y0 = 11.78816, 46.48114 # http://www.openstreetmap.org/search?query=46.48114%2C11.78816
+x1, y1 = 11.78771, 46.48165 # http://www.openstreetmap.org/search?query=46.48165%2C11.78771
+points = ((x0, y0), (x1, y1))
+x, y = zip(*(mm.rev_geocode(p) for p in points))
+ax.scatter(x, y, c='red', edgecolor='none', s=10, alpha=0.9)
+
+plt.savefig('ex-matplotlib.pdf', bbox_inches='tight')
+plt.close()
+
+# vim: sw=4:et:ai
