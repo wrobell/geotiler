65f0a01df1605ea2613fdfe73b23c06b85fba7d8
wrobell <wrobell@pld-linux.org>
2014-04-09 01:33:48 +0100

    - typo fixed

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=['Pillow'],
     classifiers=[
         'Topic :: Scientific/Engineering :: GIS',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
