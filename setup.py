4308dae80a2c2c4a1d69c00d615e2df9a042c041
wrobell <wrobell@pld-linux.org>
2015-11-04 08:13:56 +0000

    - starting to work on ver. 0.7.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     name='geotiler',
     packages=find_packages('.'),
     scripts=('bin/geotiler-lint',),
-    version='0.6.0',
+    version='0.7.0',
     description='GeoTiler - library to create maps using tiles'
         ' from a map provider',
     author='Artur Wroblewski',
