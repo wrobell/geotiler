0238a06a2e2c8390a69db5a645726aa2ad4445f8
wrobell <wrobell@pld-linux.org>
2015-11-04 08:41:19 +0000

    - removed `aiohttp` dependency from setup.py file

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author_email='wrobell@pld-linux.org',
     url='http://wrobell.it-zone.org/geotiler/',
     setup_requires = ['setuptools_git >= 1.0'],
-    install_requires=['Pillow', 'aiohttp'],
+    install_requires=['Pillow'],
     classifiers=[
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
