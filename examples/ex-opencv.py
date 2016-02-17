e2bcdb4085a851051daec726bcac90006a0214d4
wrobell <wrobell@pld-linux.org>
2016-02-17 09:29:23 +0000

    - added license header to the opencv example

--- examples/ex-opencv.py
+++ examples/ex-opencv.py
@@ -1,6 +1,30 @@
 #
-# Opencv example using geotiler
+# GeoTiler - library to create maps using tiles from a map provider
 #
+# Copyright (C) 2014-2015 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# This file incorporates work covered by the following copyright and
+# permission notice (restored, based on setup.py file from
+# https://github.com/stamen/modestmaps-py):
+#
+#   Copyright (C) 2007-2013 by Michal Migurski and other contributors
+#   License: BSD
+#
+
 """
 Create an object containing a geotiler object and convert the resulting
 image to an opencv image in order to show it.
