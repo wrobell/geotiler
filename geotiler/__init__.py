3b73124bfc47e596d891661f20f3c9622287c5ba
wrobell <wrobell@pld-linux.org>
2019-07-18 00:09:08 +0100

    - starting to work on ver. 0.14.2

--- geotiler/__init__.py
+++ geotiler/__init__.py
@@ -25,7 +25,7 @@
 #   License: BSD
 #
 
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 
 from .map import Map, render_map, render_map_async, fetch_tiles
 from .provider import find_provider, providers
