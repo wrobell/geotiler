e6307d40ecf378e85658af2f72af541781bc91c8
wrobell <wrobell@riseup.net>
2023-08-01 21:57:17 +0100

    Fix documentation config

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,6 @@ todo_include_todos = True
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_style = 'geotiler.css'
 
 # vim: sw=4:et:ai
